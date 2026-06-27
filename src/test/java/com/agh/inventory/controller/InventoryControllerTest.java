6a8f9996c57239070410a92c19cb2a370dad2c93
abhi <50333572+abhionly009@users.noreply.github.com>
2026-06-27 17:14:21 +0530

    add code to make test and spring cloud work

--- src/test/java/com/agh/inventory/controller/InventoryControllerTest.java
+++ src/test/java/com/agh/inventory/controller/InventoryControllerTest.java
@@ -6,8 +6,8 @@ import com.agh.inventory.service.InventoryService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.mockito.Mockito.when;
 
@@ -22,7 +22,7 @@ public class InventoryControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockitoBean
+    @MockBean
     InventoryService inventoryService;
 
     @Test
